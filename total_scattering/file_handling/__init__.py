447cf4a6a11f3d17c1b231a60977056b4e9f4405
Marshall McDonnell <mcdonnellmt@ornl.gov>
2020-12-28 14:59:41 -0500

    Add create_absorption_wksp to top-level file_handling

--- total_scattering/file_handling/__init__.py
+++ total_scattering/file_handling/__init__.py
@@ -1,4 +1,9 @@
-from total_scattering.file_handling.load import load
+from total_scattering.file_handling.load import load, create_absorption_wksp
 from total_scattering.file_handling.save import save_banks, save_file
 
-__all__ = ["load", "save_banks", "save_file"]
+__all__ = [
+    "create_absorption_wksp",
+    "load",
+    "save_banks",
+    "save_file",
+]
